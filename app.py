26837060fc566cf24ff2e23cfb20be9be55027ae
DELL <spanchal112000@gmail.com>
2025-08-30 21:18:14 +0530

    requirments info

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
+
+nltk.download('stopwords')
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
